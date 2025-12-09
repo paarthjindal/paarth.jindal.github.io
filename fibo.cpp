5558c85122f724375db53907a4a0e74cdf7061ef
paarthjindal <paarth.jindal@students.iiit.ac.in>
2025-12-09 15:25:10 +0530

    corrected fibo code

--- fibo.cpp
+++ fibo.cpp
@@ -2,8 +2,25 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+void fib(int n)
+{
+    cout<<"1"<<endl;
+    for(int i=2;i<=n;i++)
+    {
+        int a=0,b=1,c;
+        for(int j=2;j<=i;j++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        cout<<c<<endl;
+    }
+}
 int main()
 {
-    cout <<" ji";
+    int n;
+    cin>>n;
+    fib(n);
     return 0;
 }
